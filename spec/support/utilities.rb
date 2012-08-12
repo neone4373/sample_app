908943ff8fe96e5c05042181d6224d9d512a9687
Neo Ellison <nehemiah.ellison@gmail.com>
2012-08-12 17:29:49 -0500

    Finished 5!

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,1 @@
-
-def full_title(page_title)
-  base_title = "Neo's first Rails page!"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper

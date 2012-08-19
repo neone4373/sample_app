1cf168e116c0a87ba5ebad1f0dba804de206ab56
Neo Ellison <nehemiah.ellison@gmail.com>
2012-08-19 15:43:30 -0500

    Finish chapter 7!

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,8 @@ class User < ActiveRecord::Base
 #  before_save { |user| user.email = email.downcase }
   before_save { self.email.downcase! }
 
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name,  #presence: true, 
+	  	    length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
 	 	    format: 	{ with: VALID_EMAIL_REGEX },

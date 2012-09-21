57c4e67478959207b3d23b868341a98a4b69a701
Neo Ellison <nehemiah.ellison@gmail.com>
2012-09-21 16:48:25 -0500

    Creeping along in 11

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,15 +29,21 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }  
-  it { should respond_to(:password_confirmation) }
+#  it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
-  it { should respond_to(:authenticate) }  
+#  it { should respond_to(:authenticate) }  
 
   it { should respond_to(:admin) }
-  it { should respond_to(:authenticate) }
+#  it { should respond_to(:authenticate) }
 
   it { should be_valid }
   it { should_not be_admin }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+#  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
 
   describe "accessible attributes" do
     it "should not allow access to admin" do
@@ -193,5 +199,15 @@ describe User do
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
     end
+  end
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
   end
 end
